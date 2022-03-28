2b5139797ed89fdba9674489308d1e1684ace5ac
Abhishek <Abhishek@AbhishekMS>
2022-03-28 14:31:30 +0530

    first commit

---
+++ sample/src/test/java/practice/MedicinesTest.java
@@ -0,0 +1,21 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class MedicinesTest {
+	@Test
+	public void medicinecheck() {
+		System.out.println("Medicine checked successfully");
+	}
+	
+	@Test
+	public void medicinesupply() {
+		System.out.println("Medicine supplied successfully");
+	}
+	
+	@Test
+	public void medicinedelivery() {
+		System.out.println("Medicine delivered successfully");
+	}
+
+}
---
+++ sample/src/test/java/practice/TabletsTest.java
@@ -0,0 +1,15 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class TabletsTest {
+	@Test
+	public void tablets1() {
+		System.out.println("Tablets checked is done");
+	}
+	
+	@Test
+	public void tablets2() {
+		System.out.println("Tablets supply is done");
+	}
+}
