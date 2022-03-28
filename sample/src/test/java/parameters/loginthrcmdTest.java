5c5562189d36fe508af9b1cf6b15c4a251c1bcca
Abhishek <Abhishek@AbhishekMS>
2022-03-28 15:11:23 +0530

    second commit

---
+++ sample/src/test/java/parameters/loginthrcmdTest.java
@@ -0,0 +1,46 @@
+package parameters;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class loginthrcmdTest {
+	WebDriver driver = null;
+	@Test
+	public void login() {
+		String browser = System.getProperty("browser");
+		String url = System.getProperty("URL");
+		String username = System.getProperty("USERNAME");
+		String password = System.getProperty("PASSWORD");
+		
+		if(browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}else if (browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}else {
+			System.out.println("specify the browser");
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get(url);
+		driver.findElement(By.name("user_name")).sendKeys(username);
+		driver.findElement(By.name("user_password")).sendKeys(password);
+		driver.findElement(By.id("submitButton")).click();
+		
+		driver.findElement(By.xpath("//img[@src='themes/softed/images/user.PNG']")).click();
+		driver.findElement(By.linkText("Sign Out")).click();
+		
+		
+	}
+
+}
